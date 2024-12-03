f50e52df5b0c8f46ca92518d53a0496fbf068bdb
Jean Kaddour <jean.kaddour@icloud.com>
2024-12-03 23:25:42 +0000

    update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "PokerSettle - GBP Settlement Calculator",
-  description: "Manage your poker game settlements in British Pounds (GBP)",
+  description: "Pay Jean back quicker with this poker settlement calculator",
 };
 
 export default function RootLayout({
